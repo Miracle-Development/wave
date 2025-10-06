dd30af8ea671a29343d5bbc83be54cfe83409184
Arsen Latipov <latipovarsen2006@gmail.com>
2025-10-06 17:29:43 +0300

    [wave-4] WaveInput added to app code

--- lib/src/screens/foreground_switch_screen/paste_code_screen.dart
+++ lib/src/screens/foreground_switch_screen/paste_code_screen.dart
@@ -35,8 +35,11 @@ class _PasteCodeScreenState extends State<PasteCodeScreen> {
         SizedBox(height: 27),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: TextField(
+          child: WaveInput(
+            type: WaveInputType.code,
             controller: _codeController,
+            // TODO: Set error flag properly
+            hasError: false,
           ),
         ),
         SizedBox(height: 135),
